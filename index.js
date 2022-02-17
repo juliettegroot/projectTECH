3b2c19848692720b4c2f5af49e9fd6f2972c816e
juliettegroot <juliette.groot@hva.nl>
2022-02-17 15:09:59 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', )
+app.get('/', onHome)
+app.get('/about', onAbout)
 
+app.get('*', notFound)
+
+function onHome(req, res) {
+    res.send('hello from home')
+}
+
+function onAbout(req, res) {
+    res.send('hello from about')
+}
+
+function notFound(req, res) {
+    res.send('Not found')
+}
 
 console.log('hallo')
